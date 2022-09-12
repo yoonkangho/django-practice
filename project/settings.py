188507faff201bffe8d727173f93870ed049308d
Kangho Kenneth Yoon <yoonkangho@gmail.com>
2022-09-12 18:15:24 +0900

    feat(settings)!: set default db to postgres
    - change default database to postgres
    - add psycopg2 to dependencies
    - add postgres service to docker-compose file

--- project/settings.py
+++ project/settings.py
@@ -75,9 +75,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "project",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        'PORT': 5432,
+        "TIME_ZONE": "UTC",
     }
 }
 
